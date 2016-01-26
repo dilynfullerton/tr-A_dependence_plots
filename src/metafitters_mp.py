770a3cace3b78e58436701d92507f74bdc204bf7
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-26 11:47:32 -0800

    Added a new multi-particle meta fitter

--- src/metafitters_mp.py
+++ src/metafitters_mp.py
@@ -1,5 +1,5 @@
 from constants import FILES_DIR, PLOTS_DIR
-from fittransforms import *
+from fit_transforms import *
 from metafit import multi_particle_metafit
 
 
@@ -41,4 +41,14 @@ def multi_particle_zbt_metafit(fitfn, e_hw_pairs, **kwargs):
                                   code='mpz',
                                   xlabel='A',
                                   ylabel='Zero Body Term (MeV)',
+                                  **kwargs)
+
+
+def multi_particle_relative_metafit(fitfn, e_hw_pairs, **kwargs):
+    return multi_particle_metafit(fitfn, e_hw_pairs,
+                                  sourcedir=FILES_DIR, savedir=PLOTS_DIR,
+                                  transform=relative_y,
+                                  code='mpr',
+                                  xlabel='A',
+                                  ylabel='Relative Energy (MeV)',
                                   **kwargs)
