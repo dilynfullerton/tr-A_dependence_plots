98716475409c7f42cdd6b4f0a5e3f378c5d78e0e
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-25 17:43:35 -0800

    Added some more multi-particle metafitters

--- src/metafitters_mp.py
+++ src/metafitters_mp.py
@@ -3,12 +3,42 @@ from fittransforms import *
 from metafit import multi_particle_metafit
 
 
-def multi_particle_relative_zbt_metafit(fitfn, e_hw_pairs, **kwargs):
+def multi_particle_relative_pzbt_metafit(fitfn, e_hw_pairs, **kwargs):
     return multi_particle_metafit(fitfn, e_hw_pairs,
                                   sourcedir=FILES_DIR, savedir=PLOTS_DIR,
                                   transform=relative_zbt,
-                                  code='sprz',
+                                  code='sprpz',
                                   xlabel='A',
-                                  ylabel='Relative Single Particle Energy + '
+                                  ylabel='Relative Energy + '
                                          'Zero Body Term (MeV)',
+                                  **kwargs)
+
+
+def multi_particle_pzbt_metafit(fitfn, e_hw_pairs, **kwargs):
+    return multi_particle_metafit(fitfn, e_hw_pairs,
+                                  sourcedir=FILES_DIR, savedir=PLOTS_DIR,
+                                  transform=pzbt,
+                                  code='mppz',
+                                  xlabel='A',
+                                  ylabel='Energy + Zero Body Term (MeV)',
+                                  **kwargs)
+
+
+def multi_particle_identity_metafit(fitfn, e_hw_pairs, **kwargs):
+    return multi_particle_metafit(fitfn, e_hw_pairs,
+                                  sourcedir=FILES_DIR, savedir=PLOTS_DIR,
+                                  transform=identity,
+                                  code='mpi',
+                                  xlabel='A',
+                                  ylabel='Energy (MeV)',
+                                  **kwargs)
+
+
+def multi_particle_zbt_metafit(fitfn, e_hw_pairs, **kwargs):
+    return multi_particle_metafit(fitfn, e_hw_pairs,
+                                  sourcedir=FILES_DIR, savedir=PLOTS_DIR,
+                                  transform=zbt,
+                                  code='mpz',
+                                  xlabel='A',
+                                  ylabel='Zero Body Term (MeV)',
                                   **kwargs)
