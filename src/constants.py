04f9d7751dadbf4a512a3c1dc37d1619eac78a19
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-26 15:50:58 -0800

    Added constants regarding legend sizing

--- src/constants.py
+++ src/constants.py
@@ -9,4 +9,11 @@ P_END = '\033[0m'
 
 P_BREAK = '-' * 80
 
+# Plotting
 PLOT_CMAP = 'gnuplot'
+LEGEND_MAX_COLS = 6
+LEGEND_MAX_SPACE = .5
+LEGEND_ROWS_PER_COL = 75
+LEGEND_MAX_FONTSIZE = 14
+LEGEND_TOTAL_FONTSIZE = 480  # is divided by number of rows
+LEGEND_SPACE_SCALE = 2
