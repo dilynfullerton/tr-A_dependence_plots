155b6d400156f782c7ac0ac775e3960b6b18ca4e
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-26 15:51:23 -0800

    Minor fixes

--- src/fit_transforms_s.py
+++ src/fit_transforms_s.py
@@ -33,15 +33,19 @@ def _combine_plots(p1, p2):
     y = np.concatenate((y1, np.array(y2)))
     const_list = list()
     for c1, c2 in zip(p1[2], p2[2]):
-        if c1 == c2:
+        if c1 is not None and c2 is not None and c1 == c2:
             const_list.append(c1)
         else:
             const_list.append(None)
     const_dict = dict()
     d1, d2 = p1[3], p2[3]
     for k in set(d1.keys() + d2.keys()):
-        if k in d1 and k in d2 and d1[k] == d2[k]:
-            const_dict[k] = d1[k]
+        if k in d1 and k in d2:
+            v1, v2 = d1[k], d2[k]
+            if v1 is not None and v2 is not None and v1 == v2:
+                const_dict[k] = d1[k]
+            else:
+                const_dict[k] = None
         else:
             const_dict[k] = None
     return x, y, const_list, const_dict
