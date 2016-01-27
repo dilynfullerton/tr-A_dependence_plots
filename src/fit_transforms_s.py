f1fde9335ef213f3705f798e8ab4d18faae0e233
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-27 10:25:07 -0800

    Added ability to use combine rules to merge const_list and const_dict of the plots in s_combine_like

--- src/fit_transforms_s.py
+++ src/fit_transforms_s.py
@@ -9,13 +9,44 @@ def s_identity(plots):
     return plots
 
 
-def s_combine_like(f):
+# FUNCTIONS FOR s_combine_like
+def qnums(plot):
+    return plot[3]['qnums']
+
+
+# COMBINE RULES FOR s_combine_like
+def keep_lesser_x0_y0_zbt0_pair_in_dict(p, p1, p2):
+    const_dict = p[3]
+    cd1, cd2 = p1[3], p2[3]
+    if 'x0' in cd1 and 'x0' in cd2:
+        if cd2['x0'] < cd1['x0']:
+            const_dict['x0'] = cd2['x0']
+            const_dict['y0'] = cd2['y0'] if 'y0' in cd2 else None
+            const_dict['zbt0'] = cd2['zbt0'] if 'zbt0' in cd2 else None
+        else:
+            const_dict['x0'] = cd1['x0']
+            const_dict['y0'] = cd1['y0'] if 'y0' in cd1 else None
+            const_dict['zbt0'] = cd1['zbt0'] if 'zbt0' in cd1 else None
+    p = p[0:3] + (const_dict,)
+    return p
+
+
+def s_combine_like(f,
+                   combine_rules=list([keep_lesser_x0_y0_zbt0_pair_in_dict])):
+    """Returns a super-fit-transform that combines all plots that share the
+    same value returned by the given f, which acts on a single plot
+
+    :param f: a function which acts on a single plot and returns a comparable
+    item
+    :param combine_rules: function of the form f(plot, plot, plot) -> plot
+     that define ways that the constants lists and dicts should be merged
+    """
     def scl(plots):
         m = dict()
         for plot in plots:
             const = f(plot)
             if const in m:
-                m[const] = _combine_plots(m[const], plot)
+                m[const] = _combine_plots(m[const], plot, combine_rules)
             else:
                 m[const] = plot
         return m.values()
@@ -23,7 +54,8 @@ def s_combine_like(f):
     return scl
 
 
-def _combine_plots(p1, p2):
+def _combine_plots(p1, p2, combine_rules=None):
+    # Combine x arrays with each other and y arrays with each other
     x1, y1 = p1[0:2]
     x2, y2 = list(), list()
     for x2i, y2i in zip(*p2[0:2]):
@@ -32,6 +64,7 @@ def _combine_plots(p1, p2):
             y2.append(y2i)
     x = np.concatenate((x1, np.array(x2)))
     y = np.concatenate((y1, np.array(y2)))
+    # Combine constant lists
     const_list = list()
     for c1, c2 in zip(p1[2], p2[2]):
         if c1 is not None and c2 is not None and c1 == c2:
@@ -39,6 +72,7 @@ def _combine_plots(p1, p2):
         else:
             const_list.append(None)
     const_dict = dict()
+    # Combine constant dicts
     d1, d2 = p1[3], p2[3]
     for k in set(d1.keys() + d2.keys()):
         if k in d1 and k in d2:
@@ -49,8 +83,9 @@ def _combine_plots(p1, p2):
                 const_dict[k] = None
         else:
             const_dict[k] = None
-    return x, y, const_list, const_dict
-
-
-def qnums(plot):
-    return plot[3]['qnums']
+    # Other combine rules
+    p = x, y, const_list, const_dict
+    if combine_rules is not None:
+        for rule in combine_rules:
+            p = rule(p, p1, p2)
+    return p
