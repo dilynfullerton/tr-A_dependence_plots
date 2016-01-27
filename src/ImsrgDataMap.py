4a91cd0af4a04517f7bed3ba3ab2f852ce5c404d
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-26 17:08:17 -0800

    Added a standard index -> orbital scheme, and improved efficiency by allowing one to specify a subset of the data to parse

--- src/ImsrgDataMap.py
+++ src/ImsrgDataMap.py
@@ -14,10 +14,11 @@ class ImsrgDataMap:
     """A data structure to hold a mapping from
     (e-level, hw) -> ImsrgDatum, which will consist of all available data
     """
-    def __init__(self, parent_directory):
+    def __init__(self, parent_directory, exp_list=None, standard_indices=None):
         self.parent_dir = parent_directory
         self.map = dict()
-        # self.sub_dir_tuple_map = dict()
+        self.exp_list = exp_list
+        self.std_io_map = standard_indices
 
         self._set_maps()
 
@@ -32,9 +33,12 @@ class ImsrgDataMap:
             for f in files:
                 key = Exp(*parse.exp_from_filename(f))
 
+                if self.exp_list is not None and key not in self.exp_list:
+                    continue
+
                 if key not in self.map:
                     # self.sub_dir_tuple_map[sd] = key
-                    value = ImsrgDatum(sd, *key)
+                    value = ImsrgDatum(sd, *key, std_io_map=self.std_io_map)
                     self.map[key] = value
 
     def all_e_hw_pairs(self):
