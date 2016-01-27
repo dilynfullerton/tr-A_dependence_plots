597c1fa3e03f5b503994ade66e6a780d1a147ad2
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-27 11:32:48 -0800

    I mean, whatever, right?

--- src/main.py
+++ src/main.py
@@ -42,42 +42,42 @@ dep3_asymps = [
 ]
 
 multi_dep_asymps = [
-    combine([asymptote(2),
-             asymptotic_dependence(2, ['y0']),
-             x1_dependence(['y0'])], force_zero=17),
-    combine([asymptote(2),
-             asymptotic_dependence(2, [], [y0pzbt0]),
-             x1_dependence([], [y0pzbt0])], force_zero=17),
-    combine([asymptote(2),
-             x2_dependence(['j']),
-             x1_dependence(['j', 'tz'])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  asymptotic_dependence(2, ['y0']),
+                  x1_dependence(['y0'])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  asymptotic_dependence(2, [], [y0pzbt0]),
+                  x1_dependence([], [y0pzbt0])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  x2_dependence(['j']),
+                  x1_dependence(['j', 'tz'])], force_zero=17),
 ]
 
 mixed = [
-    combine([asymptote(2),
-             asymptote(1),
-             x1_dependence(['tz'], [joff2])], force_zero=17),
-    combine([asymptote(2),
-             x1(),
-             x1_dependence(['tz'], [joff2])], force_zero=17),
-    combine([asymptote(2),
-             x1(),
-             x1_dependence(['y0', 'zbt0'], force_zero=17)], force_zero=17),
-    combine([asymptote(2),
-             quadratic(),
-             x1_dependence(['y0'], [])], force_zero=17),
-    combine([asymptote(2),
-             quadratic(),
-             x1_dependence([], [y0pzbt0])], force_zero=17),
-    combine([asymptote(2),
-             quadratic(),
-             x1_dependence(['j', 'tz'])], force_zero=17),
-    combine([asymptote(2),
-             quadratic(),
-             x1_dependence(['tz'], [joff2])], force_zero=17),
-    combine([asymptote(2),
-             quadratic(),
-             x1_dependence(['y0', 'zbt0'])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  asymptote(1),
+                  x1_dependence(['tz'], [joff2])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  x1(),
+                  x1_dependence(['tz'], [joff2])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  x1(),
+                  x1_dependence(['y0', 'zbt0'], force_zero=17)], force_zero=17),
+    combine_ffns([asymptote(2),
+                  quadratic(),
+                  x1_dependence(['y0'], [])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  quadratic(),
+                  x1_dependence([], [y0pzbt0])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  quadratic(),
+                  x1_dependence(['j', 'tz'])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  quadratic(),
+                  x1_dependence(['tz'], [joff2])], force_zero=17),
+    combine_ffns([asymptote(2),
+                  quadratic(),
+                  x1_dependence(['y0', 'zbt0'])], force_zero=17),
 ]
 
 asymps = (simple_asymps + dep1_asymps + dep2_asymps + dep3_asymps +
@@ -90,57 +90,80 @@ max_r2(single_particle_relative_pzbt_metafit,
        print_results=False)
 '''
 
-f = combine([asymptote(2),
-             linear_dependence(['y0'])], force_zero=17)
-'''
-ans = compare(metafitter=single_particle_identity_metafit,
-              fitfn=f,
-              e_hw_pairs=[
-                  (12, 20),
-                  (14, 20),
-                  (14, 24),
-                  (12, 24, 22),
-                  (12, 24, 24)
-              ],
-              depth=2,
-              print_compare_results=True,
-              show_plot=True,
-              show_fit=False,
-              show_legend=True,
-              print_key=False,
-              print_results=False)
-'''
+f = combine_ffns([asymptote(2),
+                  linear_dependence(['y0'])], force_zero=17)
+
+# ans = compare(metafitter=single_particle_identity_metafit,
+#               fitfn=f,
+#               e_hw_pairs=[
+#                   (12, 20),
+#                   (14, 20),
+#                   (14, 24),
+#                   (12, 24, 22),
+#                   (12, 24, 24)
+#               ],
+#               depth=2,
+#               print_compare_results=True,
+#               show_plot=True,
+#               show_fit=False,
+#               show_legend=True,
+#               print_key=False,
+#               print_results=False)
 
 single_particle_firstp_metafit(
-    fitfn=combine([asymptote(2)],
-                  force_k_func=fk_to_y0
-                  ),
+    fitfn=combine_ffns([linear()],
+                       force_k_func=fk_to_y0
+                       ),
     e_hw_pairs=[
         (12, 20),
-        (14, 20),
-        (14, 24),
+        (12, 24, 22),
+        (12, 24, 24)
+    ],
+    show_plot=True,
+    show_fit=True,
+    print_key=False,
+    print_results=False
+
+)
+
+single_particle_firstp_zbt_metafit(
+    fitfn=combine_ffns([asymptote(1)],
+                       force_k_func=fk_to_zbt0
+                       ),
+    e_hw_pairs=[
+        (12, 20),
         (12, 24, 22, None),
         (12, 24, 24, None)
     ],
     show_plot=True,
     show_fit=True,
-    print_key=True,
-    print_results=True
+    print_key=False,
+    print_results=False
 )
 
-'''
 multi_particle_identity_metafit(
-        fitfn=asymptote(2, force_zero=17),
+        fitfn=asymptote(2,
+                        #force_zero=17,
+                        force_k_func=fk_to_y0),
         e_hw_pairs=[
-            #(12, 20),
-            #(14, 20),
-            #(14, 24),
+            (12, 20),
             (12, 24, 22),
             (12, 24, 24)
         ],
         show_plot=True,
-        show_fit=False,
+        show_fit=True,
         show_legend=True,
-        print_key=True,
+        print_key=False,
         print_results=False)
-'''
+
+# multi_particle_relative_pzbt_metafit(
+#     fitfn=combine_ffns([asymptote(2)], force_zero_func=fz_to_x0),
+#     e_hw_pairs=[
+#         (12, 20),
+#         #(12, 24, 22),
+#         #(12, 24, 24)
+#     ],
+#     show_plot=True,
+#     show_fit=True,
+#     show_legend=True
+# )
