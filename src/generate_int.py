470052136ee346d72d57a38d0f0b3eafc3d8a35b
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-01-27 14:54:00 -0800

    File generation function skeleton

--- src/generate_int.py
+++ src/generate_int.py
@@ -2,3 +2,21 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from constants import *
+
+
+def generate_int_file_from_fit(mf_results_sp,
+                               mf_results_mp,
+                               mf_results_zbt,
+                               mass_range=range(17, 30),
+                               file_save_dir=DIR_GEN_INT,
+                               file_save_subdir=GEN_INT_SUBDIR,
+                               file_save_name=GEN_INT_FILE_NAME,
+                               row_title=GEN_INT_ROW_TITLE,
+                               row_zbt=GEN_INT_ROW_ZERO_BODY_TERM,
+                               row_idx_key_head=GEN_INT_ROW_INDEX_KEY_HEAD,
+                               row_idx_key=GEN_INT_ROW_INDEX_KEY,
+                               row_blank=GEN_INT_ROW_BLANK,
+                               row_sp=GEN_INT_ROW_SINGLE_PARTICLE,
+                               row_mp=GEN_INT_ROW_INTERACTION):
+    pass
