fc921602a810856249a7017c282b270fd42c9fe3
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-02-02 13:21:03 -0800

    Rename Exp -> ExpInt, so as to allow more explanatory descriptive keys (Exp's) for different data file types

--- src/ExpInt.py
+++ src/ExpInt.py
@@ -3,9 +3,9 @@ from collections import namedtuple
 
 
 # noinspection PyClassHasNoInit
-class Exp(namedtuple('Exp', ['e', 'hw', 'base', 'rp'])):
+class ExpInt(namedtuple('ExpInt', ['e', 'hw', 'base', 'rp'])):
     __slots__ = ()
 
     def __str__(self):
         return str(tuple(self._asdict().values())).replace(', None', '')
-Exp.__new__.__defaults__ = (None, None)
+ExpInt.__new__.__defaults__ = (None, None)
