0765827b5bcf2eb75fbe9d5d56834ec641662df0
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-02-02 17:42:48 -0800

    Datatype for storing *.lpt body data rows

---
+++ src/Shell.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from collections import namedtuple
+
+from parse import half_int_float_to_str as to_str
+
+
+class Shell(namedtuple('Shell',
+                       ['NJ', 'E', 'Ex', 'J', 'T', 'p', 'lowest_Ex', 'name'])):
+    __slots__ = ()
+
+    def __str__(self):
+        s = '({}, {:.3}, {:.3}, {}, {}, {:+}, {:.3}, {})'
+        j_str = to_str(self.J)
+        t_str = to_str(self.T)
+        v = self._asdict().values()
+        v[3:5] = j_str, t_str
+        v[6] = self.lowest_Ex if self.lowest_Ex is not None else ''
+        return s.format(*v)
