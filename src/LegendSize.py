a98e724d9019017353703cf17fe92c5a0ab82ee4
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-02-04 09:47:53 -0800

    Major bug fixed in LegendSize. Always include division import when doing calculations!

--- src/LegendSize.py
+++ src/LegendSize.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from math import ceil
 
 
@@ -11,39 +12,20 @@ class LegendSize:
         self.rows_per_col = rows_per_col
         self.space_scale = space_scale
 
-        self.num_cols_map = dict()
-        self.fontsize_map = dict()
-        self.width_map = dict()
-
     def num_cols(self, num_plots):
-        if num_plots in self.num_cols_map:
-            return self.num_cols_map[num_plots]
-        else:
-            ans = int(
-                max(min(ceil(num_plots / self.rows_per_col), self.max_cols), 1))
-            self.num_cols_map[num_plots] = ans
-            return ans
+        return int(
+            max(min(ceil(num_plots / self.rows_per_col), self.max_cols), 1))
 
     def fontsize(self, num_plots, num_cols=None):
-        if num_plots in self.fontsize_map:
-            return self.fontsize_map[num_plots]
-        else:
-            if num_cols is None:
-                num_cols = self.num_cols(num_plots)
-            ans = min(num_cols * self.total_fontsize / num_plots,
-                      self.max_fontsize)
-            self.fontsize_map[num_plots] = ans
-            return ans
+        if num_cols is None:
+            num_cols = self.num_cols(num_plots)
+        return min(num_cols * self.total_fontsize / num_plots,
+                   self.max_fontsize)
 
     def width_scale(self, num_plots, num_cols=None, fontsize=None):
-        if num_plots in self.width_map:
-            return self.width_map[num_plots]
-        else:
-            if num_cols is None:
-                num_cols = self.num_cols(num_plots)
-            if fontsize is None:
-                fontsize = self.fontsize(num_plots, num_cols)
-            ans = (1 - (self.max_h_space * num_cols / self.max_cols) *
-                   (fontsize / self.max_fontsize) * self.space_scale)
-            self.width_map[num_plots] = ans
-            return ans
+        if num_cols is None:
+            num_cols = self.num_cols(num_plots)
+        if fontsize is None:
+            fontsize = self.fontsize(num_plots, num_cols)
+        return (1 - (self.max_h_space * num_cols / self.max_cols) *
+                (fontsize / self.max_fontsize) * self.space_scale)
