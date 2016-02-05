026167be5b4ed5046c98ef5fd966bf4c162a7cae
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-02-05 15:26:11 -0800

    New files

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from metafitters_lpt import *
 from generate_int import generate_int_file_from_fit_results
 
 # rz = single_particle_first_np_zbt_metafit(1)(
-#     fitfn=combine_ffns([linear()],
+#     fitfn=combine_ffns([asymptote(2)],
 #                        force_k_func=fk_to_zbt0),
 #     e_hw_pairs=[
 #         (12, 20),
@@ -23,29 +23,31 @@ from generate_int import generate_int_file_from_fit_results
 #     print_lr_results=False
 # )
 #
-# rsp = single_particle_first_np_metafit(1)(
-#     fitfn=combine_ffns([linear(),
-#                         # scalar_dependence(['y0'])
-#                         ],
-#                        force_k_func=fk_to_y0
-#                        ),
-#     e_hw_pairs=[
-#         (12, 20),
-#         (12, 24, 22),
-#         (12, 24, 24)
-#     ],
-#     show_plot=True,
-#     show_fit=True,
-#     print_key=False,
-#     print_results=True,
-#     print_lr_results=False
-# )
+rsp = single_particle_first_np_metafit(2)(
+    fitfn=combine_ffns([#linear(),
+                        #scalar_dependence(['y0'])
+                        linear_dependence(['y0'])
+                        ],
+                       force_k_func=fk_to_y0
+                       ),
+    e_hw_pairs=[
+        (12, 20),
+        (12, 24),
+        (12, 24, 22),
+        (12, 24, 24)
+    ],
+    show_plot=True,
+    show_fit=True,
+    print_key=False,
+    print_results=True,
+    print_lr_results=False
+)
 #
 # rmp = multi_particle_first_np_metafit(1)(
 #     fitfn=combine_ffns([linear(),
-#                         # scalar_dependence(['y0'])
+#                         scalar_dependence(['y0'])
 #                         ],
-#                        force_k_func=fk_to_y0
+#                        #force_k_func=fk_to_y0
 #                        ),
 #     e_hw_pairs=[
 #         (12, 20),
@@ -64,7 +66,7 @@ from generate_int import generate_int_file_from_fit_results
 #     e_hw_pairs=[(12, 20), (12, 24, 22), (12, 24, 24)],
 #     mass_range=range(17, 29))
 
-lpt_pzbt_for_z_in_list_and_n_in_list_metafit(list_z=[8],
+lpt_pzbt_for_z_in_list_and_n_in_list_metafit(list_z=[10],
                                              list_n=[1])(
     fitfn=linear(),
     show_plot=True
