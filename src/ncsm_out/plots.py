c7d9088abdfe324d758e4b7dd05b02790b7a6ec7
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-05 09:45:20 -0700

    Use better functions for plotting

--- src/ncsm_out/plots.py
+++ src/ncsm_out/plots.py
@@ -3,41 +3,37 @@ Functions for making plots from NCSD data
 """
 from __future__ import print_function, division, unicode_literals
 import numpy as np
+from os import path
 from constants import DPATH_SHELL_RESULTS, DPATH_NCSM_RESULTS
-from plotting import plot_the_plots, map_to_arrays
+from constants import DPATH_PLOTS_NCSMVCE
+from plotting import map_to_arrays
+from plotting import save_plot_figure, save_plot_data_file
 from ncsm_out.DataMapNcsmVceOut import DataMapNcsmVceOut
 from ncsm_vce_lpt.DataMapNcsmVceLpt import DataMapNcsmVceLpt
 
 
 def plot_ground_state_prescription_error_vs_exact(
-        a_prescriptions,
-        z=2, nmax=4, n1=15, n2=15, nshell=1, ncomponent=2,
+        a_prescriptions, z=2, nmax=4, n1=15, n2=15, nshell=1, ncomponent=2,
         abs_value=False,
         do_plot=True,
         transform=None,
         dm_exact=None, dm_vce=None,
-        dpath_shell=DPATH_SHELL_RESULTS, dpath_ncsm=DPATH_NCSM_RESULTS,
-        **kwargs
+        _dpath_shell=DPATH_SHELL_RESULTS, _dpath_ncsm=DPATH_NCSM_RESULTS,
+        _savedir=DPATH_PLOTS_NCSMVCE,
 ):
     # exact
     if dm_exact is None:
         dm_exact = DataMapNcsmVceOut(
-            parent_directory=dpath_ncsm,
-            exp_list=[(z, n1, n2)]
-        )
+            parent_directory=_dpath_ncsm, exp_list=[(z, n1, n2)])
     dat_exact = dm_exact.map.values()[0]
     ncsm_exact = dat_exact.aeff_exact_to_ground_state_energy_map(
-        nmax=nmax, nshell=nshell, ncomponent=ncomponent,
-    )
+        nmax=nmax, nshell=nshell, ncomponent=ncomponent,)
     x_ex, y_ex = [list(a) for a in map_to_arrays(ncsm_exact)]
     # A = Aeff prescription
     if dm_vce is None:
-        dm_vce = DataMapNcsmVceLpt(
-            parent_directory=dpath_shell,
-        )
+        dm_vce = DataMapNcsmVceLpt(parent_directory=_dpath_shell)
     aeff_eq_a_map = dm_vce.aeff_eq_a_to_ground_energy_map(
-        z=z, nmax=nmax, n1=n1, n2=n2, nshell=nshell, ncomponent=ncomponent,
-    )
+        z=z, nmax=nmax, n1=n1, n2=n2, nshell=nshell, ncomponent=ncomponent)
     x_aaf, y_aaf = [list(a) for a in map_to_arrays(aeff_eq_a_map)]
     x_del = sorted(list(set(x_ex) & set(x_aaf)))
     y_del = list()
@@ -79,18 +75,24 @@ def plot_ground_state_prescription_error_vs_exact(
             next_plots.append(transform(*plot))
         plots = next_plots
 
+    plots = sorted(plots, key=lambda p0: p0[3]['name'])
+    title = 'Ground state energy error due to various A-prescriptions'
+    labels = [p[3]['name'] for p in plots]
+    xlabel, ylabel = 'A', 'E_presc-E_ncsm'
+    savename = 'vce_presc{}_Nmax{}_{}_{}_shell{}_dim{}'.format(
+        str(a_prescriptions).replace(' ', ''),
+        nmax, n1, n2, nshell, ncomponent,
+    )
+
     if do_plot:
-        return plot_the_plots(
-            plots=plots,
-            title='Ground state energy error due to various A-prescriptions',
-            label='{p},'+' Nmax={}'.format(nmax),
-            xlabel='A',
-            ylabel='E_presc - E_ex',
-            get_label_kwargs=lambda p, i: {'p': p[3]['name']},
-            sort_key=lambda p: p[3]['name'],
-            include_legend=True,
-            cmap='jet',
-            **kwargs
+        save_plot_data_file(
+            plots=plots, title=title, xlabel=xlabel, ylabel=ylabel,
+            labels=labels, savepath=path.join(_savedir, savename + '.dat'),
+        )
+        return save_plot_figure(
+            data_plots=plots, title=title, xlabel=xlabel, ylabel=ylabel,
+            savepath=path.join(_savedir, savename + '.pdf'),
+            data_labels=labels,
         )
     else:
         return plots
