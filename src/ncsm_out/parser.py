6221534e4f327a4ba4217395cbbc3fa903d1c6d4
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-07 11:25:19 -0700

    Cleany

--- src/ncsm_out/parser.py
+++ src/ncsm_out/parser.py
@@ -9,28 +9,32 @@ from constants import F_PARSE_NCSMVCE_OUT_RGX_LINE_STATE as _RGX_LINE_STATE
 from constants import (F_PARSE_NCSMVCE_OUT_RGX_LINE_SPECTRUM as
                        _RGX_LINE_SPECTRUM)
 from constants import FN_PARSE_NCSMVCE_OUT_RGX_NHW as _RGX_NHW
+from constants import FN_PARSE_NCSMVCE_OUT_RGX_SCALE as _RGX_SCALE
 from constants import FN_PARSE_NCSMVCE_OUT_CHR_ELT_SPLIT as _CHR_SPLIT
-from parse import content_lines, filename_elts_list
+from parse import content_lines, filename_elts_list, elt_from_felts
 from ncsm_vce_lpt.parser import nmax_n1_n2_from_delts as nhw_n1_n2_from_felts
 
 
 # EXP
-def _z_from_filepath(filepath, _comment_str, _rgx_line_z):
-    for line in content_lines(filepath=filepath, comment_str=_comment_str):
-        if match(_rgx_line_z, line) is not None:
+def _z_from_filepath(filepath, comment_str, rgx_line_z):
+    for line in content_lines(filepath=filepath, comment_str=comment_str):
+        if match(rgx_line_z, line) is not None:
             return int(line.split()[2])
     else:
         return None
 
 
-def exp(filepath,
-        _comment_str=_CMNT_STR, _split_char=_CHR_SPLIT,
-        _rgx_line_z=_RGX_LINE_Z, _rgx_nhw=_RGX_NHW):
-    z = _z_from_filepath(filepath=filepath, _comment_str=_comment_str,
-                         _rgx_line_z=_rgx_line_z)
-    felts = filename_elts_list(filename=filepath, split_char=_split_char)
-    nhw, n1, n2 = nhw_n1_n2_from_felts(delts=felts, regex_nmax=_rgx_nhw)
-    return z, n1, n2
+def exp(filepath):
+    z = _z_from_filepath(filepath=filepath, comment_str=_CMNT_STR,
+                         rgx_line_z=_RGX_LINE_Z)
+    felts = filename_elts_list(filename=filepath, split_char=_CHR_SPLIT)
+    nhw, n1, n2 = nhw_n1_n2_from_felts(delts=felts, regex_nmax=_RGX_NHW)
+    scale_elt = elt_from_felts(felts=felts, elt_regex=_RGX_SCALE)
+    if scale_elt is None:
+        scalefactor = None
+    else:
+        scalefactor = float(scale_elt[5:])
+    return z, n1, n2, scalefactor
 
 
 # FILENAME DATA
@@ -40,33 +44,21 @@ def _a_aeff_nhw(filepath, split_char):
 
 
 # MAPS
-def a_aeff_nhw_to_states_map(
-        filepaths,
-        _split_char=_CHR_SPLIT,
-        _comment_str=_CMNT_STR,
-        _rgx_line_state=_RGX_LINE_STATE,
-        _rgx_line_spectrum=_RGX_LINE_SPECTRUM
-):
+def a_aeff_nhw_to_states_map(filepaths):
     """Based on a given list of *.out filepaths, constructs a map from
     (A, Aeff, Nhw) to the ordered list of states, each a tuple of
     energy (E), excited energy (Ex), angular momentum (J), and isospin (T).
     :param filepaths: list of filepaths from which to construct the mapping
-    :param _split_char: character that separates filename elements
-    :param _comment_str: string representing a commented line
-    :param _rgx_line_state: regular expression that matches the beginning of
-    the lines containing information about a state
-    :param _rgx_line_spectrum: regular expression that matches the beginning
-    of the lines containing a spectrum entry
     """
     a_aeff_nhw_to_states = dict()
     for f in filepaths:
-        a, aeff, nhw = _a_aeff_nhw(filepath=f, split_char=_split_char)
-        cl = content_lines(filepath=f, comment_str=_comment_str)
+        a, aeff, nhw = _a_aeff_nhw(filepath=f, split_char=_CHR_SPLIT)
+        cl = content_lines(filepath=f, comment_str=_CMNT_STR)
         state_lines, spectrum_lines = list(), list()
         for line in cl:
-            if match(_rgx_line_state, line) is not None:
+            if match(_RGX_LINE_STATE, line) is not None:
                 state_lines.append(line)
-            elif match(_rgx_line_spectrum, line) is not None:
+            elif match(_RGX_LINE_SPECTRUM, line) is not None:
                 spectrum_lines.append(line)
         states_list = list()
         for state_ln, spect_ln in zip(state_lines, spectrum_lines):
