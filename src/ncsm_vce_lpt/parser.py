3ce7305b9cc40b02a8bdb75229d3b828686b5942
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-07 11:21:40 -0700

    Retrieve scale factor from *.lpt file path from running shell on a VCE *.int

--- src/ncsm_vce_lpt/parser.py
+++ src/ncsm_vce_lpt/parser.py
@@ -13,6 +13,7 @@ from constants import FN_PARSE_NCSMVCE_LPT_RGX_PRESC as _RGX_PRESC
 from constants import FN_PARSE_NCSMVCE_LPT_RGX_NMAX as _RGX_NMAX
 from constants import FN_PARSE_NCSMVCE_LPT_RGX_NSHELL as _RGX_NSHELL
 from constants import FN_PARSE_NCSMVCE_LPT_RGX_NCOMP as _RGX_NCOMP
+from constants import FN_PARSE_NCSMVCE_LPT_RGX_SCALE as _RGX_SCALE
 from parse import elt_from_felts, filename_elts_list
 from nushellx_lpt.parser import interaction as datum_dirname
 from nushellx_lpt.parser import a_z as a_z
@@ -39,21 +40,14 @@ def nmax_n1_n2_from_delts(delts, regex_nmax):
         return tuple(nnn)
 
 
-def exp(
-        filepath,
-        _comment_str=_STR_CMNT,
-        _row_az=_ROW_AZ,
-        _split_char=_CHR_SPLIT,
-        _rgx_presc=_RGX_PRESC,
-        _rgx_nmax=_RGX_NMAX,
-        _rgx_nshell=_RGX_NSHELL,
-        _rgx_ncomp=_RGX_NCOMP,
-):
-    z = a_z(filepath=filepath, comment_str=_comment_str, row_az=_row_az)[1]
+def exp(filepath):
+    z = a_z(filepath=filepath, comment_str=_STR_CMNT, row_az=_ROW_AZ)[1]
     dirname = datum_dirname(filepath=filepath)
-    delts = filename_elts_list(filename=dirname, split_char=_split_char)
-    a_presc = _a_presc_from_delts(delts=delts, regex_presc=_rgx_presc)
-    nmax, n1, n2 = nmax_n1_n2_from_delts(delts=delts, regex_nmax=_rgx_nmax)
-    nshell = int(elt_from_felts(felts=delts, elt_regex=_rgx_nshell)[5:])
-    ncomponent = int(elt_from_felts(felts=delts, elt_regex=_rgx_ncomp)[3:])
-    return z, a_presc, nmax, n1, n2, nshell, ncomponent
+    delts = filename_elts_list(filename=dirname, split_char=_CHR_SPLIT)
+    a_presc = _a_presc_from_delts(delts=delts, regex_presc=_RGX_PRESC)
+    nmax, n1, n2 = nmax_n1_n2_from_delts(delts=delts, regex_nmax=_RGX_NMAX)
+    nshell = int(elt_from_felts(felts=delts, elt_regex=_RGX_NSHELL)[5:])
+    ncomponent = int(elt_from_felts(felts=delts, elt_regex=_RGX_NCOMP)[3:])
+    scale_elt = elt_from_felts(felts=delts, elt_regex=_RGX_SCALE)
+    scalefactor = float(scale_elt[5:]) if scale_elt is not None else None
+    return z, a_presc, nmax, n1, n2, nshell, ncomponent, scalefactor
