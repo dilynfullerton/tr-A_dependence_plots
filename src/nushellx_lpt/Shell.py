b1ec22972eb9e07b4ae1ba3ba2c8623ce8d4b40d
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-18 14:56:27 -0700

    nushellx_lpt/Shell.py : changed T to Tz, to be more specific

--- src/nushellx_lpt/Shell.py
+++ src/nushellx_lpt/Shell.py
@@ -6,7 +6,7 @@ from parse import half_int_float_to_str as to_str
 
 # noinspection PyClassHasNoInit
 class Shell(namedtuple('ShellLpt',
-                       ['NJ', 'E', 'Ex', 'J', 'T', 'p', 'lowest_Ex', 'name'])):
+                       ['NJ', 'E', 'Ex', 'J', 'Tz', 'p', 'lowest_Ex', 'name'])):
     """Holds the body data for *.lpt files
     """
     __slots__ = ()
@@ -14,7 +14,7 @@ class Shell(namedtuple('ShellLpt',
     def __str__(self):
         s = '({}, {:.3}, {:.3}, {}, {}, {:+}, {:.3}, {})'
         j_str = to_str(self.J)
-        t_str = to_str(self.T)
+        t_str = to_str(self.Tz)
         v = self._asdict().values()
         v[3:5] = j_str, t_str
         v[6] = self.lowest_Ex if self.lowest_Ex is not None else ''
