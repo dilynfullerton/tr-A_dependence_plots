fc742a8629b908602fd20e8b2f7e4f8f1a68c6a3
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-29 11:35:11 -0700

    Added parameter incl_proton, which specifies whether the proton interactions were used in the interaction

--- src/ncsm_out/ExpNcsmOut.py
+++ src/ncsm_out/ExpNcsmOut.py
@@ -4,7 +4,8 @@ from collections import namedtuple
 
 
 # noinspection PyClassHasNoInit
-class ExpNcsmOut(namedtuple('ExpNcsmVceOut', ['Z', 'n1', 'n2', 'scale'])):
+class ExpNcsmOut(namedtuple('ExpNcsmVceOut',
+                            ['Z', 'n1', 'n2', 'scale', 'incl_proton'])):
     """Identifier for a set of NCSD *.out files generated for the same
     element in the same model space
     """
@@ -13,4 +14,4 @@ class ExpNcsmOut(namedtuple('ExpNcsmVceOut', ['Z', 'n1', 'n2', 'scale'])):
     def __str__(self):
         return str(tuple(self._asdict().values())).replace(', None', '')
 
-ExpNcsmOut.__new__.__defaults__ = (None,) * 3
+ExpNcsmOut.__new__.__defaults__ = (None,) * 4
--- src/ncsm_vce_lpt/ExpNcsmVceLpt.py
+++ src/ncsm_vce_lpt/ExpNcsmVceLpt.py
@@ -6,7 +6,8 @@ from collections import namedtuple
 # noinspection PyClassHasNoInit
 class ExpNcsmVceLpt(namedtuple(
     'ExpNcsmVceLpt',
-    ['Z', 'A_presc', 'Nmax', 'n1', 'n2', 'nshell', 'ncomponent', 'scale']
+    ['Z', 'A_presc', 'Nmax', 'n1', 'n2', 'nshell', 'ncomponent',
+     'scale', 'incl_proton']
 )):
     """Identifier for a group of *.lpt results from performing NuShellX
     calculations on *.int files from a VCE
@@ -16,4 +17,4 @@ class ExpNcsmVceLpt(namedtuple(
     def __str__(self):
         return str(tuple(self._asdict().values())).replace(', None', '')
 
-ExpNcsmVceLpt.__new__.__defaults__ = (None,) * 5
+ExpNcsmVceLpt.__new__.__defaults__ = (None,) * 6
